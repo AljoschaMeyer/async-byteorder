b552d739c74952d5ceea10aa63f944b3e2970ef6
AljoschaMeyer <mail@aljoscha-meyer.de>
2017-12-08 22:19:08 +0100

    Add initial draft

---
+++ README.md
@@ -0,0 +1,4 @@
+# Async Byteorder
+Methods and fututes to asynchronously read and write numbers in either big-endian or little-endian order. Unlike the [`byteorder`](https://crates.io/crates/byteorder) crate, this correctly handles non-blocking readers and writers.
+
+[API documentation](https://docs.rs/async-byteorder/)
---
+++ src/lib.rs
@@ -0,0 +1,230 @@
+#![warn(missing_docs)]
+#[macro_use]
+extern crate futures;
+extern crate tokio_io;
+#[macro_use]
+extern crate atm_io_utils;
+
+#[cfg(test)]
+extern crate quickcheck;
+#[cfg(test)]
+extern crate partial_io;
+#[cfg(test)]
+extern crate rand;
+#[cfg(test)]
+extern crate async_ringbuffer;
+
+use std::mem::transmute;
+use std::io::Error;
+
+use futures::{Future, Async, Poll};
+use futures::Async::Ready;
+use tokio_io::{AsyncRead, AsyncWrite};
+
+pub struct ReadU32NativeOrder<R> {
+    bytes: [u8; 4],
+    offset: u8,
+    read: R,
+}
+
+impl<R> ReadU32NativeOrder<R> {
+    pub fn new(read: R) -> ReadU32NativeOrder<R> {
+        ReadU32NativeOrder {
+            bytes: [0; 4],
+            offset: 0,
+            read,
+        }
+    }
+}
+
+impl<R: AsyncRead> Future for ReadU32NativeOrder<R> {
+    type Item = u32;
+    type Error = Error;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        unimplemented!()
+    }
+}
+
+pub struct ReadU32BE<R>(ReadU32NativeOrder<R>);
+
+impl<R> ReadU32BE<R> {
+    pub fn new(read: R) -> ReadU32BE<R> {
+        ReadU32BE(ReadU32NativeOrder::new(read))
+    }
+}
+
+impl<R: AsyncRead> Future for ReadU32BE<R> {
+    type Item = u32;
+    type Error = Error;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        Ok(Ready(try_ready!(self.0.poll()).to_be()))
+    }
+}
+
+pub struct ReadU32LE<R>(ReadU32NativeOrder<R>);
+
+impl<R> ReadU32LE<R> {
+    pub fn new(read: R) -> ReadU32LE<R> {
+        ReadU32LE(ReadU32NativeOrder::new(read))
+    }
+}
+
+impl<R: AsyncRead> Future for ReadU32LE<R> {
+    type Item = u32;
+    type Error = Error;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        Ok(Ready(try_ready!(self.0.poll()).to_le()))
+    }
+}
+
+pub struct WriteU32NativeOrder<W> {
+    bytes: [u8; 4],
+    offset: u8,
+    write: W,
+}
+
+impl<W> WriteU32NativeOrder<W> {
+    pub fn new(num: u32, write: W) -> WriteU32NativeOrder<W> {
+        WriteU32NativeOrder {
+            bytes: unsafe { transmute::<u32, [u8; 4]>(num) },
+            offset: 0,
+            write,
+        }
+    }
+}
+
+impl<W: AsyncWrite> Future for WriteU32NativeOrder<W> {
+    type Item = ();
+    type Error = Error;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        unimplemented!()
+    }
+}
+
+pub struct WriteU32BE<W>(WriteU32NativeOrder<W>);
+
+impl<W> WriteU32BE<W> {
+    pub fn new(num: u32, write: W) -> WriteU32BE<W> {
+        WriteU32BE(WriteU32NativeOrder::new(num.to_be(), write))
+    }
+}
+
+impl<W: AsyncWrite> Future for WriteU32BE<W> {
+    type Item = ();
+    type Error = Error;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        self.0.poll()
+    }
+}
+
+pub struct WriteU32LE<W>(WriteU32NativeOrder<W>);
+
+impl<W> WriteU32LE<W> {
+    pub fn new(num: u32, write: W) -> WriteU32LE<W> {
+        WriteU32LE(WriteU32NativeOrder::new(num.to_le(), write))
+    }
+}
+
+impl<W: AsyncWrite> Future for WriteU32LE<W> {
+    type Item = ();
+    type Error = Error;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        self.0.poll()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use futures::Future;
+    use rand;
+    use rand::Rng;
+    use partial_io::{PartialOp, PartialAsyncRead, PartialAsyncWrite, PartialWithErrors};
+    use partial_io::quickcheck_types::GenInterruptedWouldBlock;
+    use quickcheck::{QuickCheck, StdGen};
+    use async_ringbuffer::*;
+
+    use super::*;
+
+    use std::u32;
+
+    #[test]
+    fn test_u32() {
+        let rng = StdGen::new(rand::thread_rng(), 8);
+        let mut quickcheck = QuickCheck::new().gen(rng).tests(1000);
+        quickcheck.quickcheck(test_u32_native as
+                              fn(usize,
+                                 PartialWithErrors<GenInterruptedWouldBlock>,
+                                 PartialWithErrors<GenInterruptedWouldBlock>)
+                                 -> bool);
+
+        let rng = StdGen::new(rand::thread_rng(), 8);
+        let mut quickcheck = QuickCheck::new().gen(rng).tests(1000);
+        quickcheck.quickcheck(test_u32_be as
+                              fn(usize,
+                                 PartialWithErrors<GenInterruptedWouldBlock>,
+                                 PartialWithErrors<GenInterruptedWouldBlock>)
+                                 -> bool);
+
+        let rng = StdGen::new(rand::thread_rng(), 8);
+        let mut quickcheck = QuickCheck::new().gen(rng).tests(1000);
+        quickcheck.quickcheck(test_u32_le as
+                              fn(usize,
+                                 PartialWithErrors<GenInterruptedWouldBlock>,
+                                 PartialWithErrors<GenInterruptedWouldBlock>)
+                                 -> bool);
+    }
+
+    fn test_u32_native(buf_size: usize,
+                       write_ops: PartialWithErrors<GenInterruptedWouldBlock>,
+                       read_ops: PartialWithErrors<GenInterruptedWouldBlock>)
+                       -> bool {
+        let num = u32::MAX - 1;
+
+        let (mut w, mut r) = ring_buffer(buf_size + 1);
+        let writer = WriteU32NativeOrder::new(num, &mut w);
+        let reader = ReadU32NativeOrder::new(&mut r);
+
+        let (_, read) = writer.join(reader).wait().unwrap();
+        assert_eq!(read, num);
+
+        return true;
+    }
+
+    fn test_u32_be(buf_size: usize,
+                   write_ops: PartialWithErrors<GenInterruptedWouldBlock>,
+                   read_ops: PartialWithErrors<GenInterruptedWouldBlock>)
+                   -> bool {
+        let num = u32::MAX - 1;
+
+        let (mut w, mut r) = ring_buffer(buf_size + 1);
+        let writer = WriteU32BE::new(num, &mut w);
+        let reader = ReadU32BE::new(&mut r);
+
+        let (_, read) = writer.join(reader).wait().unwrap();
+        assert_eq!(read, num);
+
+        return true;
+    }
+
+    fn test_u32_le(buf_size: usize,
+                   write_ops: PartialWithErrors<GenInterruptedWouldBlock>,
+                   read_ops: PartialWithErrors<GenInterruptedWouldBlock>)
+                   -> bool {
+        let num = u32::MAX - 1;
+
+        let (mut w, mut r) = ring_buffer(buf_size + 1);
+        let writer = WriteU32LE::new(num, &mut w);
+        let reader = ReadU32LE::new(&mut r);
+
+        let (_, read) = writer.join(reader).wait().unwrap();
+        assert_eq!(read, num);
+
+        return true;
+    }
+}
